06481f7f7a26f64c5ef7b7c511c41bafcf47098e
Benjamin Klüglein <scheibenkaes@googlemail.com>
2013-01-20 18:39:24 +0100

    init

---
+++ project.clj
@@ -0,0 +1,25 @@
+(defproject org.scheibenkaes/attic "0.1.0"
+  :description "Save stuff to local storage"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :plugins [[lein-cljsbuild "0.2.10"]]
+  :cljsbuild {:builds [{
+                        :id "dev"
+                        :source-path "src"
+                        :compiler {
+                                   :output-to "main.js"
+                                   :jar true
+                                   :optimizations :simple
+                                   :pretty-print true
+                                   }}
+                       {
+                        :id "prod"
+                        :source-path "src"
+                        :compiler {
+                                   :output-to "main.js"
+                                   :jar true
+                                   :optimizations :advanced
+                                   :pretty-print false
+                                   }}]})
---
+++ test/attic/core_test.clj
@@ -0,0 +1,7 @@
+(ns attic.core-test
+  (:use clojure.test
+        attic.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
