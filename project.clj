ceae45b3b866b00300fce01e9106b3977f6dbf4e
Benjamin Klüglein <scheibenkaes@googlemail.com>
2014-04-16 18:14:32 +0200

    version 0.1.2
    * update clojure version, because why not
    * added cljs dependency to satisfy compiler
    * ported cljsbuild data to new format

--- project.clj
+++ project.clj
@@ -1,25 +1,22 @@
-(defproject org.scheibenkaes/attic "0.1.1"
+(defproject org.scheibenkaes/attic "0.1.2"
   :description "Save stuff to local storage"
   :url "http://scheibenkaes.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :plugins [[lein-cljsbuild "0.2.10"]]
-  :cljsbuild {:builds [{
-                        :id "prod"
-                        :source-path "src"
-                        :compiler {
-                                   :output-to "main.js"
-                                   :jar true
-                                   :optimizations :advanced
-                                   :pretty-print false
-                                   }}
-                       {
-                        :id "dev"
-                        :source-path "src"
-                        :compiler {
-                                   :output-to "main.js"
-                                   :jar true
-                                   :optimizations :simple
-                                   :pretty-print true
-                                   }}]})
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2156"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
+  :cljsbuild
+  {:builds
+   [{:source-paths ["src"],
+     :compiler {:pretty-print false,
+                :output-to "main.js",
+                :optimizations :advanced,
+                :jar true},
+     :id "prod"}
+    {:source-paths ["src"],
+     :compiler {:pretty-print true,
+                :output-to "main.js",
+                :optimizations :simple,
+                :jar true},
+     :id "dev"}]})
