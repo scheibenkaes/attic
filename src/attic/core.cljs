44d23d10262717a8a1b3a4005e7038818b39dce3
Benjamin Klüglein <scheibenkaes@googlemail.com>
2017-04-02 22:09:25 +0200

    remove ^:export as it should not be needed

--- src/attic/core.cljs
+++ src/attic/core.cljs
@@ -3,20 +3,20 @@
 
 (def ^:const local-storage js/localStorage)
 
-(defn ^:export get-item
+(defn get-item
   "Retrieves a item identified by 'key'. Key can be a string or a keyword."
   [key]
   (-> (.getItem local-storage (name key))
       (or "")
       cljs.reader/read-string))
 
-(defn ^:export set-item
+(defn set-item
   "Saves a object to the local storage. 'key' can be a string or keyword."
   [key obj]
   (let [f (if (string? obj) identity pr-str)]
     (.setItem local-storage (name key) (f obj))))
 
-(defn ^:export remove-item
+(defn remove-item
   "Removes the item 'key' from the local storage."
   [key]
   (.removeItem local-storage (name key)))
