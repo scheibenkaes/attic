90667207b23ed344e842d5654236d8180852fd29
Benjamin Klüglein <scheibenkaes@googlemail.com>
2017-04-02 22:42:52 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -1,22 +1,17 @@
-(defproject org.scheibenkaes/attic "0.3.0"
+(defproject org.scheibenkaes/attic "0.4.0"
   :description "Save stuff to local storage"
   :url "http://scheibenkaes.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.107"]]
-  :plugins [[lein-cljsbuild "1.1.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.495"]]
+  :plugins [[lein-cljsbuild "1.1.5"]]
+  :hooks [leiningen.cljsbuild]
   :cljsbuild
   {:builds
-   [{:source-paths ["src"],
-     :compiler {:pretty-print false,
-                :output-to "main.js",
-                :optimizations :advanced,
-                :jar true},
-     :id "prod"}
-    {:source-paths ["src"],
-     :compiler {:pretty-print true,
-                :output-to "main.js",
-                :optimizations :simple,
-                :jar true},
-     :id "dev"}]})
+   [{:source-paths ["src"]
+     :test-paths ["test"]
+     :compiler {:pretty-print false
+                :output-to "main.js"
+                :optimizations :advanced
+                :jar true}}]})
